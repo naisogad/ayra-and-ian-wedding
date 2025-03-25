82ccabd4214e94525666dbc38db55f1c32eb94d8
Ian Dagos <ian_dagos@yahoo.com>
2025-03-26 01:30:20 +0800

    20th commit

--- app/page.js
+++ app/page.js
@@ -15,13 +15,13 @@ export default function Home() {
   return (
     <>
     <ObserverProvider>
-      <div className="mr-auto ml-auto w-full xl:max-w-[1220px] lg:max-w-[984px] md:max-w-[984px] sm:max-w-[600px]">
-        <Navbar />
-        <Header/>
-        <HeaderM/>
-        <About />
-        <AboutM />
-      </div>
+    <div className="mr-auto ml-auto w-full xl:max-w-[1220px] lg:max-w-[984px] md:max-w-[984px] sm:max-w-[600px]">
+      <Navbar />
+      <Header/>
+      <HeaderM/>
+      <About />
+      <AboutM />
+    </div>
     </ObserverProvider>
     </>
   );
