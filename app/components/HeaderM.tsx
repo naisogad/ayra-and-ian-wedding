2817cd487150c1ed12480ae2e9c63a4fdaa05807
Ian Dagos <ian_dagos@yahoo.com>
2025-03-27 14:18:07 +0800

    25th commit

--- app/components/HeaderM.tsx
+++ app/components/HeaderM.tsx
@@ -5,8 +5,8 @@ import Image from 'next/image'
 const HeaderM = () => {
   return (
     <section className='mb-0 pr-0 h-auto pt-0 min-h-[100vh] pb-[10rem] relative ml-0 mr-0 mt-15 justify-center flex flex-wrap box-border lg:hidden md:hidden'>
-        <div className='left-0 top-0 absolute w-[7rem] h-[7rem] z-1 box-border block pointer-events-none'></div>
-        <div className='right-0 top-0 absolute w-[7rem] h-[7rem] z-1 box-border block pointer-events-none'></div>
+        {/* <div className='left-0 top-0 absolute w-[7rem] h-[7rem] z-1 box-border block pointer-events-none'></div>
+        <div className='right-0 top-0 absolute w-[7rem] h-[7rem] z-1 box-border block pointer-events-none'></div> */}
         <div className='bg-[left_0.5rem] top-[5rem] h-auto min-h-[100vh] z-1 pr-0 pl-0 relative w-full box-border block'>
             {/* HEADER 1 */}
             <div className='left-[2rem] relative mb-[92px] w-[100%] text-right box-border block h-auto min-h-[20rem] motion-preset-slide-left motion-duration-1000'>
