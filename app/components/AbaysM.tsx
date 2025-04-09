1cb1b4e8356c1bae0c33746f43439a5c158c8829
Ian Dagos <ian_dagos@yahoo.com>
2025-04-09 23:06:45 +0800

    39th

--- app/components/AbaysM.tsx
+++ app/components/AbaysM.tsx
@@ -27,7 +27,7 @@ const AbaysM = () => {
   return (
     <section id='abaysM' className='flex min-h-[600px] pt-[15rem] pr-[0.5rem] pb-[3.5rem] relative justify-center flex-wrap box-border lg:hidden md:hidden'>
         {/* FLOWER 1 */}
-        <div className='w-[18rem] h-[14rem] top-[6%] left-[30%] absolute z-1 pointer-events-none box-border block'>
+        <div className='w-[18rem] h-[14rem] top-[6%] left-[31%] absolute z-1 pointer-events-none box-border block intersect-once intersect:motion-preset-slide-left motion-duration-1000'>
             <Image 
                 alt=''
                 src={'/flowers/floater_3.png'}
@@ -36,11 +36,11 @@ const AbaysM = () => {
             />
         </div>
         {/* TITLE */}
-        <div className='absolute top-0 left-0 z-1 box-border block'>
+        <div className='absolute top-0 left-0 z-1 box-border block intersect-once intersect:motion-preset-slide-up motion-duration-1000'>
             <h2 className='text-moss -rotate-90 absolute m-0 whitespace-nowrap text-ellipsis z-2 pb-[0.5rem] top-[35rem] left-[-12rem] overflow-auto text-[4rem] leading-[0.9]'>Wedding Abays</h2>
         </div>
         {/* CONTENT */}
-        <div className='z-0 pr-0 pl-0 ml-auto grow-0 shrink-0 basis-[70%] max-w-[70%] relative w-[100%] min-h-[1px] box-border block'>
+        <div className='z-0 pr-0 pl-0 ml-auto grow-0 shrink-0 basis-[70%] max-w-[70%] relative w-[100%] min-h-[1px] box-border block intersect-once intersect:motion-preset-slide-left motion-duration-1000'>
             <div className='ml-0 mr-0 justify-end flex flex-wrap box-border'>
                 <div className='grow-0 shrink-0 basis-[100%] max-w-[100%] relative w-[100%] min-h-[1px] pt-[3rem] box-border block bg-amber-50'>
                     {/* ROW */}
@@ -48,37 +48,37 @@ const AbaysM = () => {
                         <div className='grow-0 shrink-0 basis-[100%] max-w-[100%] relative w-[100%] min-h-[1px] pl-[15px] pr-[15px] box-border block'>
                             {/* LIST */}
                             <div className='box-border block text-right'>
-                                <a onClick={toggleNav} className='text-moss text-[1.9rem] leading-[1] box-border\'>
+                                <a onClick={toggleNav} onClickCapture={() => handleNavigation('ps')} className='text-moss text-[1.9rem] leading-[1] box-border\'>
                                     Primary Sponsors
                                     <p className='text-moss text-[1.125rem] leading-[1.7] break-normal overflow-hidden relative z-2 mt-0 mb-[2.5rem] block font-medium'>+ View Details</p>
                                 </a>
                             </div>
                             <div className='box-border block text-right'>
-                                <a onClick={toggleNav} className='text-moss text-[1.9rem] leading-[1] box-border'>
+                                <a onClick={toggleNav} onClickCapture={() => handleNavigation('ss')} className='text-moss text-[1.9rem] leading-[1] box-border'>
                                     Secondary Sponsors
                                     <p className='text-moss text-[1.125rem] leading-[1.7] break-normal overflow-hidden relative z-2 mt-0 mb-[2.5rem] block font-medium'>+ View Details</p>
                                 </a>
                             </div>
                             <div className='box-border block text-right'>
-                                <a onClick={toggleNav} className='text-moss text-[1.9rem] leading-[1] box-border'>
+                                <a onClick={toggleNav} onClickCapture={() => handleNavigation('bridesmaids')} className='text-moss text-[1.9rem] leading-[1] box-border'>
                                     Bridesmaid
                                     <p className='text-moss text-[1.125rem] leading-[1.7] break-normal overflow-hidden relative z-2 mt-0 mb-[2.5rem] block font-medium'>+ View Details</p>
                                 </a>
                             </div>
                             <div className='box-border block text-right'>
-                                <a onClick={toggleNav} className='text-moss text-[1.9rem] leading-[1] box-border'>
+                                <a onClick={toggleNav} onClickCapture={() => handleNavigation('groomsmen')} className='text-moss text-[1.9rem] leading-[1] box-border'>
                                     Groomsmen
                                     <p className='text-moss text-[1.125rem] leading-[1.7] break-normal overflow-hidden relative z-2 mt-0 mb-[2.5rem] block font-medium'>+ View Details</p>
                                 </a>
                             </div>
                             <div className='box-border block text-right'>
-                                <a onClick={toggleNav} className='text-moss text-[1.9rem] leading-[1] box-border'>
+                                <a onClick={toggleNav} onClickCapture={() => handleNavigation('bearers')} className='text-moss text-[1.9rem] leading-[1] box-border'>
                                     Bearers
                                     <p className='text-moss text-[1.125rem] leading-[1.7] break-normal overflow-hidden relative z-2 mt-0 mb-[2.5rem] block font-medium'>+ View Details</p>
                                 </a>
                             </div>
                             <div className='box-border block text-right'>
-                                <a onClick={toggleNav} className='text-moss text-[1.9rem] leading-[1] box-border'>
+                                <a onClick={toggleNav} onClickCapture={() => handleNavigation('parents')} className='text-moss text-[1.9rem] leading-[1] box-border'>
                                     Parents
                                     <p className='text-moss text-[1.125rem] leading-[1.7] break-normal overflow-hidden relative z-2 mt-0 mb-[2.5rem] block font-medium'>+ View Details</p>
                                 </a>
