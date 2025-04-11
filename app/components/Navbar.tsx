227d7d72b27e4174914dd09e364e952a7396a545
Ian Dagos <ian_dagos@yahoo.com>
2025-04-11 19:25:27 +0800

    65th

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -24,11 +24,13 @@ const Navbar = () => {
         {/* MENU BUTTON*/}
         <h4 className='hidden md:block fixed rotate-90 top-[4rem] right-[0rem] z-3 cursor-pointer text-3xl uppercase text-moss' onClick={toggleNav}>Menu</h4>
         {/* MOBILE BUTTON */}
-        <button className='space-y-1 fixed top-[2%] right-[3%] z-3 box-border block md:hidden' onClick={toggleNav}>
-          <span className={`block h-1 w-7 bg-moss transition-all duration-400 ease-[cubic-bezier(0.77, 0.2, 0.05, 1) ${toggleMenu? 'rotate-45 translate-y-3': ''}`}></span>
-          <span className={`block h-1 w-7 bg-moss transition-all duration-400 ease-[cubic-bezier(0.77, 0.2, 0.05, 1) ${toggleMenu? 'scale-0 opacity-0': ''}`}></span>
-          <span className={`block h-1 w-7 bg-moss transition-all duration-400 ease-[cubic-bezier(0.77, 0.2, 0.05, 1) ${toggleMenu? '-rotate-45 -translate-y-1': ''}`}></span>
-        </button>
+        <div className='w-full h-12 fixed z-1'>
+          <button className='space-y-1 fixed top-[2%] right-[3%] z-3 box-border block md:hidden' onClick={toggleNav}>
+            <span className={`block h-1 w-7 bg-moss transition-all duration-400 ease-[cubic-bezier(0.77, 0.2, 0.05, 1) ${toggleMenu? 'rotate-45 translate-y-3': ''}`}></span>
+            <span className={`block h-1 w-7 bg-moss transition-all duration-400 ease-[cubic-bezier(0.77, 0.2, 0.05, 1) ${toggleMenu? 'scale-0 opacity-0': ''}`}></span>
+            <span className={`block h-1 w-7 bg-moss transition-all duration-400 ease-[cubic-bezier(0.77, 0.2, 0.05, 1) ${toggleMenu? '-rotate-45 -translate-y-1': ''}`}></span>
+          </button>
+        </div>
         {/* NAV MENU */}
         <div className={`${toggleMenu? '': '-mr-480'} z-2 hidden md:block size-full fixed top-0 right-0 m-0 bg-white/70 transition-all duration-400 ease-[cubic-bezier(0.77, 0.2, 0.05, 1)]`}>
           <ul className={`${toggleMenu? 'mr-25': '-mr-90 '} hidden md:flex flex-col relative gap-6 w-80 max-h-89 mt-20 pb-100 pt-10 pl-10 float-right bg-stain transition-all duration-400 ease-[cubic-bezier(0.77, 0.2, 0.05, 1)]`}>
