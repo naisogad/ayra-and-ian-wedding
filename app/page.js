c5f08e0ff5f48555afa786329f5711142428b16d
Ian Dagos <ian_dagos@yahoo.com>
2025-04-11 18:13:52 +0800

    53rd

--- app/page.js
+++ app/page.js
@@ -52,7 +52,7 @@ export default function Home() {
     <>
     <ObserverProvider>
     {/* <div className="mr-auto ml-auto w-full 2x1:max-w-[1510px] xl:max-w-[1220px] lg:max-w-[984px] md:max-w-[720px] sm:max-w-[640px]"> */}
-    <div className="mr-auto ml-auto w-full 2x1:max-w-[1510px] xl:max-w-[1220px] lg:max-w-[984px] md:max-w-[560px]">
+    <div className="mr-auto ml-auto w-full 2x1:max-w-[1510px] xl:max-w-[1220px] lg:max-w-[984px] md:max-w-[500px]">
       <Navbar />
       <Header/>
       <HeaderM/>
