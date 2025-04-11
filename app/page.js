a879719a21bf8f79779ecb37d8ef077cea0c86a5
Ian Dagos <ian_dagos@yahoo.com>
2025-04-11 18:02:39 +0800

    50th

--- app/page.js
+++ app/page.js
@@ -51,7 +51,7 @@ export default function Home() {
   return (
     <>
     <ObserverProvider>
-    <div className="mr-auto ml-auto w-full xl:max-w-[1220px] lg:max-w-[984px] md:max-w-[984px] sm:max-w-[640px]">
+    <div className="mr-auto ml-auto w-full xl:max-w-[1220px] lg:max-w-[984px] md:max-w-[720px] sm:max-w-[600px]">
       <Navbar />
       <Header/>
       <HeaderM/>
